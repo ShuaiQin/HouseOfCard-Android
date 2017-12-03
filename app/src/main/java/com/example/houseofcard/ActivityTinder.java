fd768921002745d3059d2bf41e575f3dfb158f48
Manhiem <ymh2ee@outlook.com>
2017-12-03 16:01:54 -0600

    count the cards

--- app/src/main/java/com/example/houseofcard/ActivityTinder.java
+++ app/src/main/java/com/example/houseofcard/ActivityTinder.java
@@ -21,6 +21,37 @@ public class ActivityTinder extends AppCompatActivity {
     @BindView(R.id.swipeView)
     private SwipePlaceHolderView mSwipView;
 
+    String[][] strArray = {
+            {"1","a"},
+            {"2","b"},
+            {"3","c"},
+            {"4","d"},
+            {"5","e"},
+            {"6","f"},
+            {"7","g"},
+            {"8","h"},
+            {"9","i"},
+            {"10","j"},
+            {"11","k"},
+            {"12","l"},
+            {"13","m"},
+            {"14","n"},
+            {"15","o"},
+            {"16","p"},
+            {"17","q"},
+            {"18","r"},
+            {"19","s"},
+            {"20","t"},
+            {"21","u"},
+            {"22","v"},
+            {"23","w"},
+            {"24","x"},
+            {"25","y"},
+            {"26","z"},
+    };
+    int size = strArray.length;
+    int times = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,60 +70,69 @@ public class ActivityTinder extends AppCompatActivity {
                 Log.d(TAG, "onItemRemoved: " + count);
                 if(count == 0){
                     Log.d(TAG, "onItemRemoved: " + count);
-                    mSwipView.addView(new TinderCard("bias","I have a bias towards food."))
-                            .addView(new TinderCard("recital","a famous pianist's recital"))
-                            .addView(new TinderCard("swampy","The land is swampy."))
-                            .addView(new TinderCard("delectable","These food looks delectable."))
-                            .addView(new TinderCard("imitate","The girl imitates her mother."))
-                            .addView(new TinderCard("morale","Employee morale is high."))
-                            .addView(new TinderCard("decoration","There are many pretty decorations."))
-                            .addView(new TinderCard("palette","The major colors on my palette."))
-                            .addView(new TinderCard("patron","The restraurant's oldest patrons."))
-                            .addView(new TinderCard("barter","He bartered the fruits for the rabbit."));
+
+                    if(size-times*10<0){
+                        mSwipView.addView(new TinderCard("Done",""));
+                    }
+                    else if(size-times*10<10){      // next x cards (0<x<10)
+                        int temp = times*10;
+                        for(int i=temp;i<size;i++){
+                            mSwipView.addView(new TinderCard(strArray[i][0],strArray[i][1]));
+                        }
+                        times++;
+                    }
+                    else{                            // next 10 cards
+                        int temp = times*10;
+                        for(int i=0;i<10;i++){
+                            mSwipView.addView(new TinderCard(strArray[temp+i][0],strArray[temp+i][1]));
+                        }
+                        times++;
+                    }
                 }
             }
         });
 
         mSwipView.getBuilder()
-//                .setSwipeType(SwipePlaceHolderView.SWIPE_TYPE_VERTICAL)
                 .setDisplayViewCount(3)
                 .setIsUndoEnabled(true)
                 .setWidthSwipeDistFactor(4)
                 .setHeightSwipeDistFactor(6)
                 .setSwipeDecor(new SwipeDecor()
-//                        .setMarginTop(300)
-//                        .setMarginLeft(100)
-//                        .setViewGravity(Gravity.TOP)
                         .setPaddingTop(20)
                         .setSwipeMaxChangeAngle(2f)
                         .setRelativeScale(0.01f)
                         .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
                         .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
 
-        mSwipView.addView(new TinderCard("1","I have a bias towards food."))
-                .addView(new TinderCard("2","a famous pianist's recital"))
-                .addView(new TinderCard("3","The land is swampy."))
-                .addView(new TinderCard("4","These food looks delectable."))
-                .addView(new TinderCard("5","The girl imitates her mother."))
-                .addView(new TinderCard("6","Employee morale is high."))
-                .addView(new TinderCard("7","There are many pretty decorations."))
-                .addView(new TinderCard("8","The major colors on my palette."))
-                .addView(new TinderCard("9","The restraurant's oldest patrons."))
-                .addView(new TinderCard("10","He bartered the fruits for the rabbit."));
+        if(size-times*10<10){
+            int temp = times*10;
+            for(int i=temp;i<size;i++){
+                mSwipView.addView(new TinderCard(strArray[i][0],strArray[i][1]));
+            }
+
+        }
+        else{
+            mSwipView.addView(new TinderCard(strArray[0][0],strArray[0][1]));
+            mSwipView.addView(new TinderCard(strArray[1][0],strArray[1][1]));
+            mSwipView.addView(new TinderCard(strArray[2][0],strArray[2][1]));
+            mSwipView.addView(new TinderCard(strArray[3][0],strArray[3][1]));
+            mSwipView.addView(new TinderCard(strArray[4][0],strArray[4][1]));
+            mSwipView.addView(new TinderCard(strArray[5][0],strArray[5][1]));
+            mSwipView.addView(new TinderCard(strArray[6][0],strArray[6][1]));
+            mSwipView.addView(new TinderCard(strArray[7][0],strArray[7][1]));
+            mSwipView.addView(new TinderCard(strArray[8][0],strArray[8][1]));
+            mSwipView.addView(new TinderCard(strArray[9][0],strArray[9][1]));
+            times++;
+        }
+
 
         new Thread(new Runnable(){
             @Override
             public void run() {
                 try {
-                    Thread.sleep(8000);
+                    Thread.sleep(200);
                     mSwipView.enableTouchSwipe();
-//                    mSwipView.lockViews();
-//                    Thread.currentThread().sleep(4000);
-//                    mSwipView.unlockViews();
-//                    Thread.currentThread().sleep(4000);
-//                    mSwipView.lockViews();
-//                    Thread.currentThread().sleep(4000);
-//                    mSwipView.unlockViews();
+
                 }catch (InterruptedException e){
                     e.printStackTrace();
                 }
@@ -109,6 +149,5 @@ public class ActivityTinder extends AppCompatActivity {
     private void onAcceptClick(){
         mSwipView.doSwipe(true);
     }
-
 
 }
